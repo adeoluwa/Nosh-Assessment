1ea9d76924c7cb118b31540c485f311748922529
Deolu_Josef <kiishijoseph@gmail.com>
2023-08-07 13:37:18 +0100

    successful databse connection

--- server.js
+++ server.js
@@ -14,4 +14,31 @@ const dotenv = require('dotenv')
 
 const app = express()
 
-// Connect to MongoDB
+// dotenv.config()
+
+mongoose.set('strictQuery', false)
+
+// Middleware
+app.use(express.json())
+
+// DB config
+if (process.env.NODE_ENV !== 'production'){
+    dotenv.config()
+}
+
+const PORT = process.env.PORT || 8001
+
+const CONNECTION = process.env.CONNECTION_URL
+
+const mongoConnect = async () => {
+    try {
+        await mongoose.connect(CONNECTION)
+        app.listen(PORT, () => console.log('App listening on port: ' + PORT))
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+mongoConnect()
+
+// Endpoints
+app.get('/', (req, res) => res.status(200).send('Welcome'))
